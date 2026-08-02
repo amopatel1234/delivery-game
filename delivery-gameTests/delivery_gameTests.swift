0ae9f3d37c5436b4c0a68da1025f6e5f35504b5c
Amish Patel <amopatel1234@gmail.com>
2026-08-02 17:46:55 +0100

    chore: add xcode project

---
+++ delivery-gameTests/delivery_gameTests.swift
@@ -0,0 +1,16 @@
+//
+//  delivery_gameTests.swift
+//  delivery-gameTests
+//
+//  Created by Amish Patel on 24/07/2026.
+//
+
+import Testing
+
+struct delivery_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
